e6540e33923ef5590a103f9e65275d52ffe5209e
Marc-André Moreau <mamoreau@devolutions.net>
2021-09-08 15:45:59 -0400

    don't include raw CFLAGS in source file

--- crypto/buildinf.h.cmake
+++ crypto/buildinf.h.cmake
@@ -1,5 +1,5 @@
 /* auto-generated for crypto/cversion.c */
-#define CFLAGS "@CMAKE_C_COMPILER@ @CMAKE_C_FLAGS@"
+#define CFLAGS ""
 #define PLATFORM "@CMAKE_SYSTEM_NAME@"
 #define DATE "@BUILDINF_DATE@"
 static const char *compiler_flags = CFLAGS;
